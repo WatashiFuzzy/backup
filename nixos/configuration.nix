05c29af4935a474cc03c0c562739bb2fa232d849
Fuzzy <156511934+WatashiFuzzy@users.noreply.github.com>
2025-08-30 23:09:58 +0700

    Add files via upload

---
+++ nixos/configuration.nix
@@ -0,0 +1,162 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  # sddm
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  # hyprland
+  programs.hyprland.enable = true;
+  # Set your time zone.
+  time.timeZone = "Asia/Ho_Chi_Minh";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "vi_VN";
+    LC_IDENTIFICATION = "vi_VN";
+    LC_MEASUREMENT = "vi_VN";
+    LC_MONETARY = "vi_VN";
+    LC_NAME = "vi_VN";
+    LC_NUMERIC = "vi_VN";
+    LC_PAPER = "vi_VN";
+    LC_TELEPHONE = "vi_VN";
+    LC_TIME = "vi_VN";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.fuzzy = {
+    isNormalUser = true;
+    description = "Fuzzy";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.fish;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  # Firefox
+  programs.firefox.enable = true;
+  # Fish shell
+  programs.fish.enable = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  fonts = {
+  enableDefaultFonts = true; # Bật font mặc định của NixOS
+  packages = with pkgs; [
+    # Monospace đẹp kèm icon (Nerd Fonts)
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.hack
+    nerd-fonts.meslo-lg
+    nerd-fonts.roboto-mono
+
+    # Emoji + symbol
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    twemoji-color-font
+
+    # Material & Awesome (Waybar, Polybar… hay cần)
+    font-awesome
+    material-icons
+
+    # Font fallback khác
+    dejavu_fonts
+    liberation_ttf
+  ];
+};
+
+  environment.systemPackages = with pkgs; [
+    neovim 
+    wget
+    git
+    kitty
+    fastfetch
+    wofi
+    kdePackages.dolphin
+    clang
+    python314
+    gcc
+    brave
+    waybar
+    rofi
+    vesktop
+    hyprpaper
+    pavucontrol
+    grim
+    slurp
+    bottom
+    nwg-look
+    vivaldi
+  ];
+   i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;        
+      addons = with pkgs; [
+        fcitx5-unikey
+        fcitx5-configtool           
+        fcitx5-gtk                  
+        libsForQt5.fcitx5-qt        
+      ];
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
